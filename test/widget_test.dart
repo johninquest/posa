a3dd609e68d4c8bb97f2dd3bf5b8deccfb352ad6
John <john-francis.edah@tech11.com>
2023-12-21 12:41:22 +0100

    updated app name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tepr/main.dart';
+import 'package:repr/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
