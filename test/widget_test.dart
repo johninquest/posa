b98e43958c31a0262a724024dfa64f3cd5b61bb2
John <john-francis.edah@tech11.com>
2024-01-19 15:26:57 +0100

    initial commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:repr/main.dart';
+import 'package:posapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
