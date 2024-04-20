17b5712566e0116d07593ef45d6997696c055704
john@home <johninquest@gmail.com>
2024-04-21 01:16:16 +0200

    updated code base

--- lib/style/colors.dart
+++ lib/style/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Color primaryColor = Color(0xff81C784);
+const Color primaryColor = Color(0xff00997b);
 // const Color primaryColor = Color(0xff6667ab);
 // final primaryColor2 = Colors.blue[900];
 const Color secondaryColor = Color(0xFFfff83b);
